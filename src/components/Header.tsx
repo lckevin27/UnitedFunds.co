b93385a26a665b5a3bc90363bcc190fb37f4d069
lckevin27 <kevin27.cl@gmail.com>
2021-03-09 13:25:39 +0800

    Complete side nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,12 +23,49 @@ const Header = () => {
     setStratDropdown(false);
   }
 
+  const [showSideNav, setSideNav] = useState(false);
+
+  const openNav = () => {
+    setSideNav(true);
+  }
+
+  const closeNav = () => {
+    setSideNav(false);
+  }
+
   return (
     <header>
       <nav onMouseLeave={disableDropdowns}>
         <div className="logo">
           <Link to="/"><img src={logo} alt="logo"/></Link>
         </div>
+        <ul className="side-nav" style={ showSideNav ? { width: '40%' } : { width: '0' } }>
+          <li><a href="javascript:void(0)" onClick={ closeNav }>&#10005;</a></li>
+          <li><Link to="/" onClick={ closeNav }>首页</Link></li>
+          <li className="dropdown">
+            <Link to="/about" onClick={ closeNav }>关于联合资本 &#9660;</Link>
+            <div className={`dropdown-menu`}>
+              <HashLink className="dropdown-item" to="/about#concepts" onClick={ closeNav }>初心与理念</HashLink>
+              <HashLink className="dropdown-item" to="/about#team" onClick={ closeNav }>核心团队</HashLink>
+            </div>
+          </li>
+          <li className="dropdown">
+            <Link to="/strategies" onClick={ closeNav }>投资策略 &#9660;</Link>
+            <div className={`dropdown-menu`}>
+              <HashLink className="dropdown-item" to="/strategies#advantages" onClick={ closeNav }>矿业投资的特点</HashLink>
+              <HashLink className="dropdown-item" to="/strategies#our-strategies" onClick={ closeNav }>联合资本的投资策略</HashLink>
+            </div>
+          </li>
+          <li className="dropdown">
+            <Link to="/cases" onClick={ closeNav }>精选案例 &#9660;</Link>
+            <div className={`dropdown-menu`}>
+              <HashLink className="dropdown-item" to="/cases#AML" onClick={ closeNav }>Anova Metals Limited</HashLink>
+              <HashLink className="dropdown-item" to="/cases#GL" onClick={ closeNav }>Global Lithium</HashLink>
+              <HashLink className="dropdown-item" to="/cases#GPC" onClick={ closeNav }>Global Potash Corporation</HashLink>
+            </div>
+          </li>
+          <li><Link to="/contact" onClick={ closeNav }>联系方式</Link></li>
+        </ul>
         <ul className="nav-links">
           <li><Link to="/" onMouseEnter={disableDropdowns}>首页</Link></li>
           <li className="dropdown">
@@ -39,14 +76,14 @@ const Header = () => {
             </div>
           </li>
           <li className="dropdown">
-            <li><Link to="/strategies" onMouseEnter={() => enableDropdown('strat')}>投资策略</Link></li>
+            <Link to="/strategies" onMouseEnter={() => enableDropdown('strat')}>投资策略</Link>
             <div className={`dropdown-menu ${stratDropdown ? "" : 'hidden'}`} onMouseLeave={disableDropdowns}>
               <HashLink className="dropdown-item" to="/strategies#advantages">矿业投资的特点</HashLink>
               <HashLink className="dropdown-item" to="/strategies#our-strategies">联合资本的投资策略</HashLink>
             </div>
           </li>
           <li className="dropdown">
-            <li><Link to="/cases" onMouseEnter={() => enableDropdown('cases')}>精选案例</Link></li>
+            <Link to="/cases" onMouseEnter={() => enableDropdown('cases')}>精选案例</Link>
             <div className={`dropdown-menu ${casesDropdown ? "" : 'hidden'}`} onMouseLeave={disableDropdowns}>
               <HashLink className="dropdown-item" to="/cases#AML">Anova Metals Limited</HashLink>
               <HashLink className="dropdown-item" to="/cases#GL">Global Lithium</HashLink>
@@ -54,6 +91,13 @@ const Header = () => {
             </div>
           </li>
           <li><NavLink activeClassName="is-active" to="/contact" onMouseEnter={disableDropdowns}>联系方式</NavLink></li>
+          <li className="open-nav" >
+            <a href="javascript:void(0)" onClick={ openNav }>
+              <div></div>
+              <div></div>
+              <div></div>
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
